5e4dc52d69aea81d0458155ffd340ccb1288d42f
Douglas Webster <douglas.webster2107@gmail.com>
2022-04-07 17:00:36 +0100

    Final iteration for darts.jl

--- darts/darts.jl
+++ darts/darts.jl
@@ -1,3 +1,10 @@
 function score(x, y)
+    # don't calculate the radius yourself use hypot it's safer.
+    r = hypot(x, y)
 
+    # take advantage of short circuit operators
+    r > 10 && return 0
+    r > 5 && return 1
+    r > 1 && return 5
+    return 10
 end
