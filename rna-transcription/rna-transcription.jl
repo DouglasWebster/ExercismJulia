0072b7096103ace614c519aee6820f21b21566a1
Douglas Webster <douglas.webster2107@gmail.com>
2022-04-08 08:56:41 +0100

    Final iteration for rna-transciption.jl

--- rna-transcription/rna-transcription.jl
+++ rna-transcription/rna-transcription.jl
@@ -1,4 +1,20 @@
+"""
+Given: DNA strand
+
+Return: It's RNA compliment
+
+The RNA compliment is given by
+
+G -> C, C -> G, T -> A, A -> U
+
+"""
 function to_rna(dna)
-
+    complement = Dict('G'=>'C', 'C'=>'G', 'T'=>'A', 'A'=>'U')
+    rna = ""
+    try
+        [rna *= complement[x] for x in dna]
+    catch
+        throw(ErrorException("Invalid nucleotide"))
+    end
+    return rna
 end
-
