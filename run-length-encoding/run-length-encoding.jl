e39215c50dd81c2c091eab3f9204dad8ff55ef92
Douglas Webster <douglas.webster2107@gmail.com>
2022-04-08 11:08:35 +0100

    final iteration for run-length-encoding

--- run-length-encoding/run-length-encoding.jl
+++ run-length-encoding/run-length-encoding.jl
@@ -1,7 +1,23 @@
 function encode(s)
+   code = ""
+
+    for matched in eachmatch(r"(.)\1*", s)
+        len = length(matched.match)
+        code *= (len > 1 ? "$(len)" : "") * "$(matched[1])"
+    end
+
+    return code
 end
 
 
 
 function decode(s)
+    code = ""
+    for matches in eachmatch(r"(\d*)(\D)", s)
+        len = (matches[1] == "" ? 1 : parse(Int64, matches[1]))
+        code *= repeat(matches[2], len)
+    end
+
+    return code
+
 end
