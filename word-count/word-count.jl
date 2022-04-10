d775ec51fc3b2ce1dc3fd6b79d7be7698a2126d8
Douglas Webster <douglas.webster2107@gmail.com>
2022-04-10 16:10:55 +0100

    final iteration for word-count

--- word-count/word-count.jl
+++ word-count/word-count.jl
@@ -1,3 +1,15 @@
-function wordcount(sentence)
+#=
+Now lets try and do it with one regex expression!
+    first get all words => \w+
+    now the optional '{character} => ('\w)?
+=#
 
+function wordcount(sentence)
+    occurances = Dict{String, Int64}()
+    for m in eachmatch(r"\w+('\w)?", sentence)
+        occurances[lowercase(m.match)] = get(occurances, lowercase(m.match), 0) + 1
+    end
+    return occurances
+    
 end
+
