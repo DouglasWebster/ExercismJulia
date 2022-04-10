fef1c47ae71e0e673508ada032f78b765276d0e3
Douglas Webster <douglas.webster2107@gmail.com>
2022-04-10 17:27:03 +0100

    final iteration of etl.jl

--- etl/etl.jl
+++ etl/etl.jl
@@ -1,4 +1,11 @@
 function transform(input::AbstractDict)
+    output = Dict()
+    for (key, letters) in input
+        for letter in letters
+            output[lowercase(letter)] = key
+        end
+    end
+
+    return output
 
 end
-
