af88cbb3ebc0eb60ec56c44c42a40e1b57814704
Douglas Webster <douglas.webster2107@gmail.com>
2022-04-11 10:14:00 +0100

    final iteration of reverse-string

--- reverse-string/reverse-string.jl
+++ reverse-string/reverse-string.jl
@@ -1,1 +1,10 @@
+using Unicode
+
+const TEST_GRAPHEMES = true
+
+# now lets make it concises the julia way by collecting the individual
+# graphemes, passing them to reverse and then rejoining
+# and because it's concise by using the assignment form
+
+myreverse(sentence::String) = graphemes(sentence) |> collect |> reverse |> join
 
