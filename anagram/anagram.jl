7a42edd197c21ad6cc041ab5ed900843d67013d9
Douglas Webster <douglas.webster2107@gmail.com>
2022-04-12 10:00:40 +0100

    Final iteration of anagram.jl

--- anagram/anagram.jl
+++ anagram/anagram.jl
@@ -1,3 +1,5 @@
 function detect_anagrams(subject, candidates)
-
+    s = sort(collect(lowercase(subject)))
+    [c for c in candidates if (lowercase(c) != lowercase(subject))
+        && (sort(collect(lowercase(c))) == s) ]
 end
