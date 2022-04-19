7699534902f7b72d1d080050b13fcb84e09ebf01
Douglas Webster <douglas.webster2107@gmail.com>
2022-04-19 16:28:16 +0100

    minesweeper iteration 1

--- minesweeper/minesweeper.jl
+++ minesweeper/minesweeper.jl
@@ -1,18 +1,27 @@
+
 function annotate(grid::AbstractArray)
     ((rows = length(grid)) == 0 || (cols = length(grid[1])) == 0) && return grid
-    chargrid = [collect(grid[r]) for r in 1:rows]
-    output = zeros(Int, rows, cols)
+    chargrid = permutedims(reshape(collect(join(grid)), cols, rows))
+    dummy = fill(' ', (rows+2, cols +2))
+    mines = findall(c -> c=='*', chargrid)
 
-    for r in 1:rows
-        for c in 1:cols
-            r-1 > 0 && c-1 > 0 
-        end
+    for mine in mines
+        dummy[mine[1], mine[2]] == ' ' ? dummy[mine[1], mine[2]] = '1' : dummy[mine[1], mine[2]] += 1
+        dummy[mine[1], mine[2]+1] == ' ' ? dummy[mine[1], mine[2]+1] = '1' : dummy[mine[1], mine[2]+1] += 1
+        dummy[mine[1], mine[2]+2] == ' ' ? dummy[mine[1], mine[2]+2] = '1' : dummy[mine[1], mine[2]+2] += 1
+        dummy[mine[1]+1, mine[2]] == ' ' ? dummy[mine[1]+1, mine[2]] = '1' : dummy[mine[1]+1, mine[2]] += 1
+        dummy[mine[1]+1, mine[2]+2] == ' ' ? dummy[mine[1]+1, mine[2]+2] = '1' : dummy[mine[1]+1, mine[2]+2] += 1
+        dummy[mine[1]+2, mine[2]] == ' ' ? dummy[mine[1]+2, mine[2]] = '1' : dummy[mine[1]+2, mine[2]] += 1
+        dummy[mine[1]+2, mine[2]+1] == ' ' ? dummy[mine[1]+2, mine[2]+1] = '1' : dummy[mine[1]+2, mine[2]+1] += 1
+        dummy[mine[1]+2, mine[2]+2] == ' ' ? dummy[mine[1]+2, mine[2]+2] = '1' : dummy[mine[1]+2, mine[2]+2] += 1
+    end
+    for mine in mines
+        dummy[mine[1]+1, mine[2]+1] = '*'
     end
 
-    return output
-end
+    required_values = [dummy[i, j] for i in 2:size(dummy, 1)-1, j in 2:size(dummy, 2)-1]
+    output = [join(required_values[i, :]) for i in 1:size(required_values,1)]
 
-minefield = ["   ",
-" * ",
-"   "]
-annotate(minefield)
+    return output
+
+end
