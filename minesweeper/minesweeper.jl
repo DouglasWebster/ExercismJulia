a71d676e208fd653ad85b972891cb1d06d7cf4c8
Douglas Webster <douglas.webster2107@gmail.com>
2022-04-19 18:11:53 +0100

    minesweeper final iteration

--- minesweeper/minesweeper.jl
+++ minesweeper/minesweeper.jl
@@ -1,27 +1,30 @@
 
-function annotate(grid::AbstractArray)
-    ((rows = length(grid)) == 0 || (cols = length(grid[1])) == 0) && return grid
-    chargrid = permutedims(reshape(collect(join(grid)), cols, rows))
-    dummy = fill(' ', (rows+2, cols +2))
-    mines = findall(c -> c=='*', chargrid)
+function annotate(grid)
+    ((rows = length(grid)) == 0 || (cols = length(first(grid))) == 0) && return grid
 
+    mines = findall([grid[x][y] == '*' for x in 1:rows, y in 1:cols])
+    
+    # create a dummy matrix with a border to accept out of range items
+    dummy = fill(' ', (rows+2, cols +2))
+ 
     for mine in mines
-        dummy[mine[1], mine[2]] == ' ' ? dummy[mine[1], mine[2]] = '1' : dummy[mine[1], mine[2]] += 1
-        dummy[mine[1], mine[2]+1] == ' ' ? dummy[mine[1], mine[2]+1] = '1' : dummy[mine[1], mine[2]+1] += 1
-        dummy[mine[1], mine[2]+2] == ' ' ? dummy[mine[1], mine[2]+2] = '1' : dummy[mine[1], mine[2]+2] += 1
-        dummy[mine[1]+1, mine[2]] == ' ' ? dummy[mine[1]+1, mine[2]] = '1' : dummy[mine[1]+1, mine[2]] += 1
-        dummy[mine[1]+1, mine[2]+2] == ' ' ? dummy[mine[1]+1, mine[2]+2] = '1' : dummy[mine[1]+1, mine[2]+2] += 1
-        dummy[mine[1]+2, mine[2]] == ' ' ? dummy[mine[1]+2, mine[2]] = '1' : dummy[mine[1]+2, mine[2]] += 1
-        dummy[mine[1]+2, mine[2]+1] == ' ' ? dummy[mine[1]+2, mine[2]+1] = '1' : dummy[mine[1]+2, mine[2]+1] += 1
-        dummy[mine[1]+2, mine[2]+2] == ' ' ? dummy[mine[1]+2, mine[2]+2] = '1' : dummy[mine[1]+2, mine[2]+2] += 1
+        for x in 0:2
+            for y in 0:2
+                if x != 1 || y != 1
+                    dummy[mine[1]+x, mine[2]+y] == ' ' ?
+                        dummy[mine[1]+x, mine[2]+y] = '1' : 
+                        dummy[mine[1]+x, mine[2]+y] += 1
+                end
+            end
+        end
     end
-    for mine in mines
+
+    # replace the mines in the dummy matrix
+    for mine in mines 
         dummy[mine[1]+1, mine[2]+1] = '*'
     end
 
-    required_values = [dummy[i, j] for i in 2:size(dummy, 1)-1, j in 2:size(dummy, 2)-1]
-    output = [join(required_values[i, :]) for i in 1:size(required_values,1)]
-
-    return output
+    # now all we need is the center of the dummy with each row joined into a string
+    map(join, eachrow([dummy[i, j] for i in 2:size(dummy, 1)-1, j in 2:size(dummy, 2)-1]))
 
 end
