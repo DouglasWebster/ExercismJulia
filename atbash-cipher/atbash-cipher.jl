630c39f307896d0b5f057181afa01942f3bb2098
Douglas Webster <douglas.webster2107@gmail.com>
2022-04-23 16:14:25 +0100

    final iteration of atbash-cipher

--- atbash-cipher/atbash-cipher.jl
+++ atbash-cipher/atbash-cipher.jl
@@ -1,8 +1,17 @@
 function encode(input)
+    stripped_input = filter(c -> (isletter(c) || isdigit(c)), lowercase(input))
+    output = ""
+    
+    for n in 1:length(stripped_input)
+        x = stripped_input[n]
+        isletter(x) ? y = 'm' - x + 'n' : y = x
+        n % 5 == 0 ? output *= (y * ' ') : output *= y
+    end
+
+    return strip(output)
 
 end
 
 function decode(input)
-
+    filter(c -> !isspace(c), encode(input))    
 end
-
