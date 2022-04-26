b2c1d5404f0488c48f598532d4b35164dca562b4
Douglas Webster <douglas.webster2107@gmail.com>
2022-04-26 17:36:43 +0100

    final iteration of sieve of Eratosthenes

--- sieve/sieve.jl
+++ sieve/sieve.jl
@@ -1,3 +1,13 @@
 function sieve(limit)
-
+    s = fill(true, limit)
+    s[1] = false
+    for n=2:ceil(Int, sqrt(limit))
+        if s[n]
+            for k = 2n:n:limit
+                s[k] = false
+            end
+        end
+    end
+    findall(s)
 end
+
