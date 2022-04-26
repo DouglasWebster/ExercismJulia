859f0ebb55e91317117f40d91ad7bba33ea95533
Douglas Webster <douglas.webster2107@gmail.com>
2022-04-26 14:12:03 +0100

    final iteration for grains

--- grains/grains.jl
+++ grains/grains.jl
@@ -1,9 +1,7 @@
-"""Calculate the number of grains on square `square`."""
-function on_square(square)
+"""Calculate the number of grains on square `square`."""    
+on_square(square) = (square < 1 || square > 64) ? throw(DomainError(square)) : big(2) ^ (square -1)    
 
-end
 
 """Calculate the total number of grains after square `square`."""
-function total_after(square)
+total_after(square) = (square < 1 || square > 64) ? throw(DomainError(square)) : big(2) ^ square -1
 
-end
