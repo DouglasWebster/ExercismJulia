04ded281814df4c6752ea60c3a7c02869745f90c
Douglas Webster <douglas.webster2107@gmail.com>
2022-04-28 15:56:38 +0100

    final iteration for trinary exercise

--- trinary/trinary.jl
+++ trinary/trinary.jl
@@ -1,3 +1,11 @@
-function trinary_to_decimal(str)
-
+function trinary_to_decimal(str::String)
+    isempty(filter(x -> (x < '0' || x > '2'), str)) || return 0
+    i = 0
+    result = 0
+    for n ∈ reverse(str)
+        result += (n - '0') * 3 ^i
+        i += 1
+    end
+    result
 end
+
