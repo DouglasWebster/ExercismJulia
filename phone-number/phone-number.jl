101c0980b9a30f7c63022da3e8f5fe28237ea835
Douglas Webster <douglas.webster2107@gmail.com>
2022-05-14 12:37:39 +0100

    final iteration of phone-number exercise.

--- phone-number/phone-number.jl
+++ phone-number/phone-number.jl
@@ -1,3 +1,7 @@
 function clean(phone_number)
-    
+    dgts = reverse(digits(parse(Int, filter(isdigit, phone_number))))
+    dgts[1] == 1 && (dgts = dgts[2:end])
+    (length(dgts) ≠ 10 || dgts[1] < 2 || dgts[4] < 2) &&
+            throw(ArgumentError("$dgts is not valid"))
+    return join(string.(dgts))
 end
