4d067f5520f66b8fe042f96632e14216fa6b40c7
Douglas Webster <douglas.webster2107@gmail.com>
2022-05-14 18:22:04 +0100

    Final iteration for triangle exercise.

--- triangle/triangle.jl
+++ triangle/triangle.jl
@@ -1,8 +1,16 @@
-function is_equilateral(sides)
+inequal(side) = (side[1] + side[2]) > side[3]
+
+function is_equilateral(sides):: Bool
+    !inequal(sort(sides)) && return false
+    length(unique(sides)) == 1
 end
 
 function is_isosceles(sides)
+    !inequal(sort(sides)) && return false
+    length(unique(sides)) < 3
 end
 
 function is_scalene(sides)
+    !inequal(sort(sides)) && return false
+    length(unique(sides)) == 3
 end
