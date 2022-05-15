7a1c84413381a50775ea73146a48c2d58af8e4e8
Douglas Webster <douglas.webster2107@gmail.com>
2022-05-15 16:23:16 +0100

    Final iteration of resistor-color-trio exercise.

--- resistor-color-trio/resistor-color-trio.jl
+++ resistor-color-trio/resistor-color-trio.jl
@@ -1,3 +1,26 @@
-function label(colors)
-    
-end
+values = Dict(
+    "black" => "0",
+    "brown" => "1",
+    "red" => "2",
+    "orange" => "3",
+    "yellow" => "4",
+    "green" => "5",
+    "blue" => "6",
+    "violet" => "7",
+    "grey" => "8",
+    "white" => "9"
+)
+
+suffix = Dict(
+    "black" => " ohms",
+    "brown" => "0 ohms",
+    "red" => "00 ohms",
+    "orange" => " kiloohms",
+    "yellow" => "0 kiloohms"
+)
+
+
+label(colors) = colors[2] == "black"  && colors[3] == "red"  ?
+        value = values[colors[1]] * suffix["orange"] :
+        value = values[colors[1]] * values[colors[2]] * suffix[colors[3]]
+
