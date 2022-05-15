26d48ec345ffcbece805bda1aacae009924597a2
Douglas Webster <douglas.webster2107@gmail.com>
2022-05-15 18:02:51 +0100

    final iteration of perfect-number exercise.

--- perfect-numbers/perfect-numbers.jl
+++ perfect-numbers/perfect-numbers.jl
@@ -1,1 +1,8 @@
+aliquot_sum(n::Int) = n < 1 && 
+                        throw(DomainError("$n is not a positive natural number")) ||
+                        sum([x for x in 1:n÷2 if n % x == 0])
 
+
+isperfect(num::Int)::Bool = aliquot_sum(num) == num
+isabundant(num::Int)::Bool = aliquot_sum(num) > num
+isdeficient(num::Int)::Bool = aliquot_sum(num) < num
