0004ebb05448854b581f3f4a30e2c04a7dc4bb5b
Douglas Webster <douglas.webster2107@gmail.com>
2022-05-15 12:13:56 +0100

    Final iteration for bob exercise.

--- bob/bob.jl
+++ bob/bob.jl
@@ -1,3 +1,10 @@
 function bob(stimulus)
-
+    stimulus = strip(stimulus)
+    length(stimulus) == 0 && return "Fine. Be that way!"
+    letters = filter(c -> islowercase(c) || isuppercase(c), stimulus)
+    shout = length(letters) > 0 && all(c -> isuppercase(c), letters,)
+    stimulus[end] == '?' && shout && return "Calm down, I know what I'm doing!"
+    shout && return "Whoa, chill out!"
+    stimulus[end] == '?' && return "Sure."
+    "Whatever."
 end
