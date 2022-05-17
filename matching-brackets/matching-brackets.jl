fa028e9c257e522db300fc5028fe4b87b625442c
Douglas Webster <douglas.webster2107@gmail.com>
2022-05-17 15:13:45 +0100

    Final iteration for matching-brackets exercise.

--- matching-brackets/matching-brackets.jl
+++ matching-brackets/matching-brackets.jl
@@ -1,1 +1,20 @@
+"""
+    matching_brackets(s)
+    
+Given a string `s` containing brackets `[]`, braces `{}`, parentheses `()`, 
+or any combination thereof, return `true` ifany and all pairs are matched 
+and nested correctly else `false`.
+"""
+function matching_brackets(s::String) :: Bool
+    buffer = []
+    for c ∈ s
+        c == '[' && push!(buffer, ']')
+        c == '{' && push!(buffer, '}')
+        c == '(' && push!(buffer, ')')
+        (c == ']' || c == '}' || c == ')') && 
+        (isempty(buffer) || c ≠ pop!(buffer)) && return false
+    end
+    return isempty(buffer)
+end
 
+matching_brackets("{[]}")
