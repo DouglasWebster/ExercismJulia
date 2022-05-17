b4ce734548d211b0444ba0b0f7398e14c53d722f
Douglas Webster <douglas.webster2107@gmail.com>
2022-05-17 17:16:41 +0100

    final iteration of pythagorean-triples exercise.

--- pythagorean-triplet/pythagorean-triplet.jl
+++ pythagorean-triplet/pythagorean-triplet.jl
@@ -1,1 +1,12 @@
+function pythagorean_triplets(n::Int)
+    triplets = []
+    for x ∈ 1:n÷3+1
+        for y ∈ x+1:n÷2+1
+            z = n - x - y
+            z^2 == (x^2 + y^2) && push!(triplets, (x, y, z))
+        end
+    end
 
+    return triplets
+
+end
